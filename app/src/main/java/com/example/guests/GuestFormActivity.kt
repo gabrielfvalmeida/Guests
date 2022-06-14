ca7703c77fb086301342037bb951019d12d17d28
binogabriel <gabrielfv.almeida@gmail.com>
2022-06-14 13:05:27 -0300

    V2

--- app/src/main/java/com/example/guests/GuestFormActivity.kt
+++ app/src/main/java/com/example/guests/GuestFormActivity.kt
@@ -2,10 +2,31 @@ package com.example.guests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.guests.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.button_save){
+
+        }
     }
 }
