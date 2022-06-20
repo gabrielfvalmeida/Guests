7e34622a3ebef018f56863b80df4358b66634dda
binogabriel <gabrielfv.almeida@gmail.com>
2022-06-20 17:29:11 -0300

    20/06

--- app/src/main/java/com/example/guests/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/guests/repository/GuestDataBase.kt
@@ -13,7 +13,8 @@ class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VE
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("create table " + DataBaseConstants.GUEST.TABLE_NAME + " (" + DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, " + DataBaseConstants.GUEST.COLUMNS.NAME + " text, " + DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
+        db.execSQL("CREATE TABLE " + DataBaseConstants.GUEST.TABLE_NAME + " (" + DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, " + DataBaseConstants.GUEST.COLUMNS.NAME + " text, " + DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
---
+++ app/src/main/java/com/example/guests/repository/GuestRepository.kt
@@ -0,0 +1,80 @@
+package com.example.guests.repository
+
+import android.content.ContentValues
+import android.content.Context
+import com.example.guests.constants.DataBaseConstants
+import com.example.guests.model.GuestModel
+
+class GuestRepository private constructor(context: Context) {
+
+    private val guestDataBase = GuestDataBase(context)
+
+    //Singleton
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository {
+
+            if (!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
+    fun insert(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val values = ContentValues()
+
+            val presence = if (guest.presence) 1 else 0
+
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, values)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+
+    }
+
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val values = ContentValues()
+            val presence = if (guest.presence) 1 else 0
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
